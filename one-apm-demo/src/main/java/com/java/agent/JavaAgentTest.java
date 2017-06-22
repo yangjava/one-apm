dcfbe177759c019ff50cc63a12eb1df84c62c6cb
Yangjing <Yangjing@Yangjing-PC>
2017-06-22 10:14:12 +0800

    提交 one-apm-agent test

--- one-apm-demo/src/main/java/com/java/agent/JavaAgentTest.java
+++ one-apm-demo/src/main/java/com/java/agent/JavaAgentTest.java
@@ -1,5 +1,12 @@
 package com.java.agent;
 
+import java.lang.instrument.Instrumentation;
+//com.java.agent.JavaAgentTest
 public class JavaAgentTest {
-
+	
+	 public static void premain(String agentArgument, 
+	            Instrumentation instrumentation) { 
+	        System.out.println("Test Java Agent"); 
+	    } 
+	 
 }
